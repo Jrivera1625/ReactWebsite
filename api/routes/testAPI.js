f5c00b8ac5fdbb5122b98b75c4325c4a18b296f7
dhruv1754 <dhruv1754@gmail.com>
2019-09-29 14:30:11 -0700

    new project with boiler plate and new personal website tempelate

--- api/routes/testAPI.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-router.get('/', function(req, res, next) {
-    console.log("did stuff")
-    res.send('API is working properly');
-});
-
-module.exports = router;
-
