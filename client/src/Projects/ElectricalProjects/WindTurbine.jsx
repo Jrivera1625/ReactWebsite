a989e9683a4eee7b17fb453c89d6e2f8df050ae2
Jarvin <j.rivera1625@hotmail.com>
2020-04-21 18:14:25 -0700

    commit

--- client/src/Projects/ElectricalProjects/WindTurbine.jsx
+++ client/src/Projects/ElectricalProjects/WindTurbine.jsx
@@ -23,20 +23,15 @@ class WindTurbine extends Component {
     return (
   
       <div>
-        <div style={{ textAlign: "center" }}>
-          <header class="pt-5 pb-3">
-            <small style={{ fontFamily: "suisse intl, Helvetica, microsoft yahei, sans-serif" }} >
-              ELEC 391
-</small>
-            <h1 class="bold" style={{
-              fontSize: "3em", fontWeight: "700"
-              , fontFamily: "suisse intl, Helvetica, microsoft yahei,sans-serif"
-            }} >Wind Turbine Design Report</h1>
-            <p  class = "container narrow" style={{ borderBottom: '1px solid', textAlign: "center" }}>Created 2019-04-18</p>
-          </header>
           <div class="container narrow">
-          <iframe src = "../../../../../Report.pdf" style={{width:"100%",height:"700px", overflow:"auto"}} >
-          </iframe>
+            <iframe src = "https://drive.google.com/file/d/1veg42QAzzyJkUrUf946fi4mCHaUWvOBT/preview" style={{width:"100%",height:"700px", overflow:"auto"}} >
+          </iframe> 
+     {/*     <object data="https://drive.google.com/file/d/1veg42QAzzyJkUrUf946fi4mCHaUWvOBT/preview"  width="600" height="500">    
+           <embed src="https://drive.google.com/file/d/1veg42QAzzyJkUrUf946fi4mCHaUWvOBT/preview" width="600px" height="500px"/>       
+             <p>This browser does not support PDFs. Please download the PDF to view it:
+                <a href="https://drive.google.com/file/d/1veg42QAzzyJkUrUf946fi4mCHaUWvOBT/preview">View the PDF</a></p>     
+             
+                </object> */}
 {/*            <ul style={{ listStyleType: "none", lineHeight: '1.1', textAlign: "left" }} >
               <li><a style={{color:"blue"}} href ="youtube.com">1.0 Project Description </a></li>
 
@@ -49,7 +44,6 @@ class WindTurbine extends Component {
 
 
           </ul> */}
-          </div>
           </div>
           </div>
 
